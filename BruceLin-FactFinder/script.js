154a3576a63afad51bb15a37761c86a95fad4e49
Brucelcr <98186796+Brucelcr@users.noreply.github.com>
2022-01-21 12:15:16 -0800

    Add files via upload

---
+++ BruceLin-FactFinder/script.js
@@ -0,0 +1,27 @@
+function b1(){
+	document.querySelector('#facts').innerHTML ="<b>There's a company that turns dead bodies into an ocean reef.</b><br><image src='images/coralreef.jpg' alt='coralreef'>";
+}
+function b2(){
+	document.querySelector('#facts').innerHTML="<b>The name bonobo resulted from a misspelling.<b><br><image src='images/bonobo.jpg' alt='bonobo'>";
+}
+function b3(){
+	document.querySelector('#facts').innerHTML="<b>There is an annual Coffee Break Festival.</b><br><image src='images/coffe.jpg' alt='coffe'>";
+}
+function b4(){
+	document.querySelector('#facts').innerHTML="<b>You can buy a flying bicycle.</b><br><image src='images/bike.jpg' alt='bike'>";
+}
+function b5(){
+	document.querySelector('#facts').innerHTML="<b>Dolphins sleep with one eye open.</b><br><image src='images/dolphin.jpg' alt='dolphin'>";
+}
+function t1(){
+	document.querySelector('body').style.backgroundImage = "";
+	document.querySelector('body').style.backgroundColor = "#E6E6E6";
+}
+function t2(){
+	document.querySelector('body').style.backgroundImage = "";
+	document.querySelector('body').style.backgroundColor = "#39393A";
+}
+function t3(){
+	document.querySelector('body').style.backgroundImage = "url('images/stome.jpg')";
+	document.querySelector('body').style.backgroundSize = "2450px";
+}
