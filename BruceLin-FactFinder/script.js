e96936a2563565fd9e76e17a82adbbcdfc6f3a87
Brucelcr <98186796+Brucelcr@users.noreply.github.com>
2022-01-21 23:22:46 -0800

    Add files via upload

--- BruceLin-FactFinder/script.js
+++ BruceLin-FactFinder/script.js
@@ -16,12 +16,54 @@ function b5(){
 function t1(){
 	document.querySelector('body').style.backgroundImage = "";
 	document.querySelector('body').style.backgroundColor = "#E6E6E6";
+	document.querySelector('body').style.inputColor = "#E6E6E6";
+	document.getElementById('a1').style.backgroundColor = "#74226C";
+	document.querySelector('#a1').style.color = "#97EAD2";
+	document.getElementById('a2').style.backgroundColor = "#74226C";
+	document.querySelector('#a2').style.color = "#97EAD2";
+	document.getElementById('a3').style.backgroundColor = "#74226C";
+	document.querySelector('#a3').style.color = "#97EAD2";
+	document.getElementById('a4').style.backgroundColor = "#74226C";
+	document.querySelector('#a4').style.color = "#97EAD2";
+	document.getElementById('a5').style.backgroundColor = "#74226C";
+	document.querySelector('#a5').style.color = "#97EAD2";
+
+	document.querySelector('#facts').style.color = "#E86A92";
+	document.querySelector('#facts').style.backgroundColor = "#886F68";
 }
 function t2(){
 	document.querySelector('body').style.backgroundImage = "";
-	document.querySelector('body').style.backgroundColor = "#39393A";
+	document.querySelector('body').style.backgroundColor = "#0A2342";
+	
+	document.getElementById('a1').style.backgroundColor = "#816E94";
+	document.querySelector('#a1').style.color = "#8CC7A1";
+	document.getElementById('a2').style.backgroundColor = "#816E94";
+	document.querySelector('#a2').style.color = "#8CC7A1";
+	document.getElementById('a3').style.backgroundColor = "#816E94";
+	document.querySelector('#a3').style.color = "#8CC7A1";
+	document.getElementById('a4').style.backgroundColor = "#816E94";
+	document.querySelector('#a4').style.color = "#8CC7A1";
+	document.getElementById('a5').style.backgroundColor = "#816E94";
+	document.querySelector('#a5').style.color = "#8CC7A1";
+	
+	document.querySelector('#facts').style.color = "#424C55";
+	document.querySelector('#facts').style.backgroundColor = "#D1CCDC";
 }
 function t3(){
 	document.querySelector('body').style.backgroundImage = "url('images/stome.jpg')";
 	document.querySelector('body').style.backgroundSize = "2450px";
+	
+	document.getElementById('a1').style.backgroundColor = "#9FD356";
+	document.querySelector('#a1').style.color = "#FA824C";
+	document.getElementById('a2').style.backgroundColor = "#9FD356";
+	document.querySelector('#a2').style.color = "#FA824C";
+	document.getElementById('a3').style.backgroundColor = "#9FD356";
+	document.querySelector('#a3').style.color = "#FA824C";
+	document.getElementById('a4').style.backgroundColor = "#9FD356";
+	document.querySelector('#a4').style.color = "#FA824C";
+	document.getElementById('a5').style.backgroundColor = "#9FD356";
+	document.querySelector('#a5').style.color = "#FA824C";
+	
+	document.querySelector('#facts').style.color = "#7E4E60";
+	document.querySelector('#facts').style.backgroundColor = "#C0F8D1"; 
 }
